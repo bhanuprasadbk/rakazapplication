7f452ffe16f740783a2853f61b665926a3b71d32
Bhanuprasad <bhanu@rpwebapps.com>
2025-08-28 15:40:59 +0530

    code comitted

--- rakaz-multi-modules/src/environments/environment.prod.ts
+++ rakaz-multi-modules/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-    authUrl: 'https://rakazapp1.azurewebsites.net/api/auth/',  
-    apiUrl: 'https://rakazapp1.azurewebsites.net/api/'
+    authUrl: 'https://rakazapp1.azurewebsites.net:8080/api/auth/',  
+    apiUrl: 'https://rakazapp1.azurewebsites.net:8080/api/'
 };
