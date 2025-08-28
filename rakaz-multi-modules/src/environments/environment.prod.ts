7acc44d486756d9a126c209d1d4663836783c1b8
bhanuprasadbk <55533460+bhanuprasadbk@users.noreply.github.com>
2025-08-28 10:47:13 +0530

    Update environment.prod.ts

--- rakaz-multi-modules/src/environments/environment.prod.ts
+++ rakaz-multi-modules/src/environments/environment.prod.ts
@@ -1,9 +1,5 @@
 export const environment = {
   production: true,
-    // authUrl: 'http://192.168.1.58:3002/api/auth/',
-    // apiUrl: 'http://192.168.1.58:3002/api/'
-    // authUrl: 'http://rpwebapps.us:8087/api/auth/',
-    // apiUrl: 'http://rpwebapps.us:8087/api/'
     authUrl: 'https://rakazapp-e6aqdrh7adgpbzcn.centralus-01.azurewebsites.net/api/auth/',  
     apiUrl: 'https://rakazapp-e6aqdrh7adgpbzcn.centralus-01.azurewebsites.net/api/'
 };
